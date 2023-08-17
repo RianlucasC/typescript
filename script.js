868e1fc037af4ef34f9cf567e93afadd071eebd2
Rianlucas7 <rianlucas592@gmail.com>
2023-08-17 19:02:13 -0300

    Annotation

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+let produto = "Uma string";
+let preco = 1;
+const carro = {
+    marca: "audi",
+    preco: 75000,
+    disponivel: true,
+};
+/*
+carro.marca = 10;
+produto = 10;
+*/
---
+++ script.ts
@@ -0,0 +1,19 @@
+let produto: string = "Uma string";
+let preco: number = 1;
+
+const carro: {
+    marca: string;
+    preco: number;
+    disponivel: boolean;
+} = {
+    marca : "audi",
+    preco: 75000,
+    disponivel: true,
+}
+
+
+/*
+carro.marca = 10;
+produto = 10;
+*/
+
