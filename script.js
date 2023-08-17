506dfa8e3b806b711ecd4e9c72117a896c65b59b
Rianlucas7 <rianlucas592@gmail.com>
2023-08-17 19:18:28 -0300

    inference

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ const carro = {
     preco: 75000,
     disponivel: true,
 };
+const barato = preco < 400 ? true : "produto caro";
+function somar(a, b) {
+    return a + b;
+}
+somar(1, 1);
 /*
+somar('3',2);
 carro.marca = 10;
 produto = 10;
 */
--- script.ts
+++ script.ts
@@ -1,18 +1,23 @@
-let produto: string = "Uma string";
-let preco: number = 1;
+let produto = "Uma string";
+let preco = 1;
 
-const carro: {
-    marca: string;
-    preco: number;
-    disponivel: boolean;
-} = {
+const carro = {
     marca : "audi",
     preco: 75000,
     disponivel: true,
 }
 
+const barato = preco < 400? true: "produto caro";
+
+function somar(a: number, b: number) {
+    return a + b;
+}
+
+somar(1, 1);
+
 
 /*
+somar('3',2);
 carro.marca = 10;
 produto = 10;
 */
